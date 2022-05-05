6cc99fd83d8a5c8734887811b03b8f7bbfdc3cc5
rlflorencio <rafaellflorencio@outlook.com>
2022-05-04 23:34:26 -0300

    Fixes "Movimentacao" date and time implementation.

--- src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -2,8 +2,12 @@ package com.dio.santander.banklineapi.repository;
 
 import com.dio.santander.banklineapi.model.Movimentacao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+	
+	public List<Movimentacao>findByIdConta(Integer idConta);
 
 }
